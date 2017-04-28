6923d97b369b99163ed95c3cd6766d2ed4aab5d0
Morteza1615 <mori13796@gmail.com>
2017-04-28 10:42:18 -0700

    morteza1615

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "262361297:AAHfT2fXHS4eP7hsYzlVk7JuDyeW7EXdgkY"
+bot_token = "285985426:AAEzkoxPQkHCkZNIXp2TA3O4up3Rd-yH8qw"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.1"
-sudo_id = 92777063
+sudo_id = 191128096
